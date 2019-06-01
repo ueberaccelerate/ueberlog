6d468be5820871330a840c3f03897f5eb890778b
ueberaccelerate <ueberaccelerate@gmail.com>
2019-06-01 10:49:50 +0300

    ** wrap common functionality to class **

--- include/ueberlog/logger.hpp
+++ include/ueberlog/logger.hpp
@@ -11,7 +11,6 @@ namespace ueberlog {
     Logger(Logger &&) = delete;
     Logger operator=(const Logger&) = delete;
     Logger operator=(Logger&&) = delete;
-  
     public:
       static Logger &i() {
         static Logger inst;
@@ -29,33 +28,33 @@ namespace ueberlog {
       template<typename ...Args>
       void debug( const char* function_name, const int line, const char *message, Args&& ...args ) const {
         if( level <= Level::debug ) {
-          print_log_level(Level::debug, Color::blue, get_timestamp(), function_name, line, message, std::forward<Args&&>(args)...);
+          ULogger::i().print_log_level(Level::debug, Color::blue, function_name, line, message, std::forward<Args&&>(args)...);
         }
       }
   
       template<typename ...Args>
       void info( const char* function_name, const int line, const char *message, Args&& ...args ) const {
         if( level <= Level::info ) {
-          print_log_level(Level::info, Color::cyan, get_timestamp(), function_name, line, message, std::forward<Args&&>(args)...);
+          ULogger::i().print_log_level(Level::info, Color::cyan, function_name, line, message, std::forward<Args&&>(args)...);
         }
       }
       template<typename ...Args>
       void warn( const char* function_name, const int line, const char *message, Args&& ...args ) const {
         if( level <= Level::warn ) {
-          print_log_level(Level::warn, Color::yellow, get_timestamp(), function_name, line, message, std::forward<Args&&>(args)...);
+          ULogger::i().print_log_level(Level::warn, Color::yellow, function_name, line, message, std::forward<Args&&>(args)...);
         }
       }
       template<typename ...Args>
       void error( const char* function_name, const int line, const char *message, Args&& ...args ) const {
         if( level <= Level::error ) {
-          print_log_level(Level::error, Color::red, get_timestamp(), function_name, line, message, std::forward<Args&&>(args)...);
+          ULogger::i().print_log_level(Level::error, Color::red, function_name, line, message, std::forward<Args&&>(args)...);
         }
       }
       template<typename ...Args>
       void throw_assert(const bool condition, const char *function_name, const int line, const char *message, Args&& ...args) const {
         if( !isrelease && !condition ) {
           {
-            print_log_level(Level::assert, Color::red, get_timestamp().c_str(), function_name, line, message, std::forward<Args&&>(args)...);
+            ULogger::i().print_log_level(Level::assert, Color::red, function_name, line, message, std::forward<Args&&>(args)...);
           }
           assert(condition);
         }
--- include/ueberlog/uebercommon.hpp
+++ include/ueberlog/uebercommon.hpp
@@ -24,15 +24,22 @@ namespace ueberlog {
       yellow,// \u001b[33m
       reset  // \u001b[0m
     };
-    void set_color ( Type color ) const {
-      const std::array<const char*, 6> colors_ {"\u001b[31m", "\u001b[32m", "\u001b[34m", "\u001b[36m", "\u001b[33m", "\u001b[0m"};
-      printf("%s", colors_[color] );
-    }
+    Color() = delete;
+    Color(const Color &) = delete;
+    Color(Color &&) = delete;
+    Color operator=(const Color&) = delete;
+    Color operator=(Color&&) = delete;
+
+
     explicit Color (const Type color) {
       set_color(color);
     }
     ~Color() {
       set_color(reset);
+    }
+    void set_color ( Type color ) const {
+      const std::array<const char*, 6> colors_ {"\u001b[31m", "\u001b[32m", "\u001b[34m", "\u001b[36m", "\u001b[33m", "\u001b[0m"};
+      printf("%s", colors_[color] );
     }
   };
   enum class Level {
@@ -42,54 +49,64 @@ namespace ueberlog {
     error,
     assert
   };
-  UEBERLOG_INLINE std::string level_to_string(const Level level) {
-    switch(level) {
-      case Level::debug:  return "DEBUG";
-      case Level::info:   return "INFO";
-      case Level::warn:   return "WARN";
-      case Level::error:  return "ERROR";
-      case Level::assert: return "ASSERT";
-      default: return "";
-    }
-  }
-  UEBERLOG_INLINE std::string get_timestamp() {
-    auto now = std::chrono::system_clock::now();
-    std::time_t now_c = std::chrono::system_clock::to_time_t(now - std::chrono::hours(24));
-    std::stringstream ss;
-    ss << std::put_time(std::localtime(&now_c), "%F %T");
-    return ss.str();
-  }
-  template <typename ...Args>
-  UEBERLOG_INLINE void print( const char * const message, Args&& ...args) {
-    char *point = const_cast<char *>(message);
-    int argscount{0};
-    while(*point != '\0') {
-      if( *point == '%' ) argscount++;
-      point++;
-    }
-    if( argscount == sizeof...(args) ) {
-      std::printf (message, std::forward<Args&&>(args)...);
-    }
-  }
-  template<>
-  UEBERLOG_INLINE void print( const char *message) {
-    std::printf ("%s", message);
-  }
+  class ULogger {
+    ULogger() = default;
+    ULogger(const ULogger &) = delete;
+    ULogger(ULogger &&) = delete;
+    ULogger operator=(const ULogger&) = delete;
+    ULogger operator=(ULogger&&) = delete;
 #ifdef THREAD_SAFE
- namespace {
-  std::mutex logger_mutex;// = []() -> std::mutex& {static std::mutex inst; return inst; }();
- }
+    std::mutex &logger_mutex = []() -> std::mutex& {static std::mutex inst; return inst; }();
 #endif
+    public:
+    static ULogger &i() {
+      static ULogger inst;
+      return inst;
+    }
+    UEBERLOG_INLINE std::string level_to_string(const Level level) {
+      switch(level) {
+        case Level::debug:  return "DEBUG";
+        case Level::info:   return "INFO";
+        case Level::warn:   return "WARN";
+        case Level::error:  return "ERROR";
+        case Level::assert: return "ASSERT";
+        default: return "";
+      }
+    }
+    UEBERLOG_INLINE std::string get_timestamp() {
+      auto now = std::chrono::system_clock::now();
+      std::time_t now_c = std::chrono::system_clock::to_time_t(now - std::chrono::hours(24));
+      std::stringstream ss;
+      ss << std::put_time(std::localtime(&now_c), "%F %T");
+      return ss.str();
+    }
+    template <typename ...Args>
+    UEBERLOG_INLINE void print( const char * const message, Args&& ...args) {
+      char *point = const_cast<char *>(message);
+      int argscount{0};
+      while(*point != '\0') {
+        if( *point == '%' ) argscount++;
+        point++;
+      }
+      if( argscount == sizeof...(args) ) {
+        std::printf (message, std::forward<Args&&>(args)...);
+      }
+    }
+    template<>
+    UEBERLOG_INLINE void print( const char *message) {
+      std::printf ("%s", message);
+    }
 
-  template<typename ...Args>
-  UEBERLOG_INLINE void print_log_level(const Level level, const Color::Type &color, const std::string& timestamp,
-                        const char* function_name, const int line, const char *message, Args&& ...args) {
+    template<typename ...Args>
+    UEBERLOG_INLINE void print_log_level(const Level level, const Color::Type &color,
+                          const char* function_name, const int line, const char *message, Args&& ...args) {
 #ifdef THREAD_SAFE
-    [[maybe_unused]] std::lock_guard<std::mutex> lk(logger_mutex);
+      [[maybe_unused]] std::lock_guard<std::mutex> lk(logger_mutex);
 #endif
-    Color c{color};
-    std::printf( "%s [%s %s:%d]:", timestamp.c_str(), level_to_string(level).c_str(), function_name, line );
-    print( message, std::forward<Args&&>(args)... );
-  }
+      Color c{color};
+      std::printf( "%s [%s %s:%d]:", get_timestamp().c_str(), level_to_string(level).c_str(), function_name, line );
+      print( message, std::forward<Args&&>(args)... );
+    }
+  };
 }
 #endif // UEBERLOG_COMMON_HPP
