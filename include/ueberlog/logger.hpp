3ef153a738431228bf3c13ad121979e6101ec741
ueberaccelerate <ueberaccelerate@gmail.com>
2019-06-19 12:08:57 +0300

    ** formated code (using clang-format) **

--- include/ueberlog/logger.hpp
+++ include/ueberlog/logger.hpp
@@ -4,57 +4,74 @@
 #include <ueberlog/uebercommon.hpp>
 
 namespace ueberlog {
-  /* easy logger */
-  class Logger : public nocopimovable{
-    Logger() = default;
-    public:
-      static Logger &i() {
-        static Logger inst;
-        return inst;
-      }
-      private:
+/* easy logger */
+class Logger : public nocopimovable {
+  Logger() = default;
+
+ public:
+  static Logger& i() {
+    static Logger inst;
+    return inst;
+  }
+
+ private:
 #if defined NDEBUG
-      bool isrelease{true};
-      Level level{Level::error};
+  bool isrelease{true};
+  Level level{Level::error};
 #else
-      bool isrelease{false};
-      Level level{Level::debug};
+  bool isrelease{false};
+  Level level{Level::debug};
 #endif
-      public:
-      template<typename ...Args>
-      void debug( const char* function_name, const int line, const char *message, Args&& ...args ) const {
-        if( level <= Level::debug ) {
-          ULogger::i().print_log_level(Level::debug, Color::blue, function_name, line, message, std::forward<Args&&>(args)...);
-        }
+ public:
+  template <typename... Args>
+  void debug(const char* function_name, const int line, const char* message,
+             Args&&... args) const {
+    if (level <= Level::debug) {
+      ULogger::i().print_log_level(Level::debug, Color::blue, function_name,
+                                   line, message,
+                                   std::forward<Args&&>(args)...);
+    }
+  }
+
+  template <typename... Args>
+  void info(const char* function_name, const int line, const char* message,
+            Args&&... args) const {
+    if (level <= Level::info) {
+      ULogger::i().print_log_level(Level::info, Color::cyan, function_name,
+                                   line, message,
+                                   std::forward<Args&&>(args)...);
+    }
+  }
+  template <typename... Args>
+  void warn(const char* function_name, const int line, const char* message,
+            Args&&... args) const {
+    if (level <= Level::warn) {
+      ULogger::i().print_log_level(Level::warn, Color::yellow, function_name,
+                                   line, message,
+                                   std::forward<Args&&>(args)...);
+    }
+  }
+  template <typename... Args>
+  void error(const char* function_name, const int line, const char* message,
+             Args&&... args) const {
+    if (level <= Level::error) {
+      ULogger::i().print_log_level(Level::error, Color::red, function_name,
+                                   line, message,
+                                   std::forward<Args&&>(args)...);
+    }
+  }
+  template <typename... Args>
+  void throw_assert(const bool condition, const char* function_name,
+                    const int line, const char* message, Args&&... args) const {
+    if (!isrelease && !condition) {
+      {
+        ULogger::i().print_log_level(Level::assert, Color::red, function_name,
+                                     line, message,
+                                     std::forward<Args&&>(args)...);
       }
-  
-      template<typename ...Args>
-      void info( const char* function_name, const int line, const char *message, Args&& ...args ) const {
-        if( level <= Level::info ) {
-          ULogger::i().print_log_level(Level::info, Color::cyan, function_name, line, message, std::forward<Args&&>(args)...);
-        }
-      }
-      template<typename ...Args>
-      void warn( const char* function_name, const int line, const char *message, Args&& ...args ) const {
-        if( level <= Level::warn ) {
-          ULogger::i().print_log_level(Level::warn, Color::yellow, function_name, line, message, std::forward<Args&&>(args)...);
-        }
-      }
-      template<typename ...Args>
-      void error( const char* function_name, const int line, const char *message, Args&& ...args ) const {
-        if( level <= Level::error ) {
-          ULogger::i().print_log_level(Level::error, Color::red, function_name, line, message, std::forward<Args&&>(args)...);
-        }
-      }
-      template<typename ...Args>
-      void throw_assert(const bool condition, const char *function_name, const int line, const char *message, Args&& ...args) const {
-        if( !isrelease && !condition ) {
-          {
-            ULogger::i().print_log_level(Level::assert, Color::red, function_name, line, message, std::forward<Args&&>(args)...);
-          }
-          assert(condition);
-        }
-      }
-  };
-}
-#endif //UEBERLOG_LOGGER_HPP
+      assert(condition);
+    }
+  }
+};
+}  // namespace ueberlog
+#endif  // UEBERLOG_LOGGER_HPP
